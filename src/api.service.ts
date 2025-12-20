e69411b1156d264afa7bfceb462ca083639ab1fa
ValeriiKorchahin <83233179+ValeriiKorchahin@users.noreply.github.com>
2025-12-20 22:43:15 +0200

    added api service, environments set up, changed prettier run

---
+++ src/api.service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+
+  private readonly httpClient = inject(HttpClient);
+  private readonly apiUrl = environment.url;
+
+  get<T>(url: string, options = {}): Observable<T> {
+    return this.httpClient.get<T>(this.apiUrl + url, options);
+  }
+
+  post<B, R>(url: string, body: B, options = {}): Observable<R> {
+    return this.httpClient.post<R>(this.apiUrl + url, body, options);
+  }
+
+  patch<B, R>(url: string, body: B, options = {}): Observable<R> {
+    return this.httpClient.patch<R>(this.apiUrl + url, body, options);
+  }
+
+  put<B, R>(url: string, body: B, options = {}): Observable<R> {
+    return this.httpClient.put<R>(this.apiUrl + url, body, options);
+  }
+
+  delete<T>(url: string, options = {}): Observable<T> {
+    return this.httpClient.delete<T>(this.apiUrl + url, options);
+  }
+}
