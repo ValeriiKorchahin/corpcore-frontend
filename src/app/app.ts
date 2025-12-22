a9e6b411b7f74f2fd31aa1b524cf939dde3b0a7e
ValeriiKorchahin <83233179+ValeriiKorchahin@users.noreply.github.com>
2025-12-22 16:13:31 +0200

    moved some files, css => scss migration

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+}
