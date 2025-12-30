015ed4082f5110892f2fbc9636ed610f83961450
ValeriiKorchahin <valerakorchagin1512@gmail.com>
2025-12-30 22:56:36 +0200

    added logout and menu with settings

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -5,10 +5,21 @@ import { MatTooltip } from '@angular/material/tooltip';
 import { MatFormField } from '@angular/material/input';
 import { MatOption, MatSelect } from '@angular/material/select';
 import { UserService } from '../../services/user.service';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconButton, MatIcon, MatTooltip, MatFormField, MatSelect, MatOption],
+  imports: [
+    MatIconButton,
+    MatIcon,
+    MatTooltip,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatMenu,
+    MatMenuItem,
+    MatMenuTrigger,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -16,4 +27,8 @@ export class HeaderComponent {
   public readonly userService = inject(UserService);
   public currentUser = computed(() => this.userService.user());
   public companies = computed(() => this.userService.userCompanies());
+
+  logout() {
+    this.userService.logout();
+  }
 }
