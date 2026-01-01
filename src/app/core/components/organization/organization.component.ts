e6e856899bf30dcd572f6a065f82833b0c703618
ValeriiKorchahin <83233179+ValeriiKorchahin@users.noreply.github.com>
2026-01-01 19:10:15 +0200

    organization component: styles and nav bar added

---
+++ src/app/core/components/organization/organization.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject, signal } from '@angular/core';
+import { MatButton, MatFabButton } from '@angular/material/button';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatCard } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-organization',
+  imports: [
+    MatButton,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatCard,
+    MatFabButton,
+    MatIcon
+  ],
+  templateUrl: './organization.component.html',
+  styleUrl: './organization.component.scss',
+})
+export class OrganizationComponent {
+
+  public readonly navigation = signal([
+    {
+      title: 'Companies',
+      icon: 'source_environment',
+      link: '/companies'
+    },
+    {
+     title: 'Users',
+      icon: 'supervisor_account',
+     link: '/users'
+    },
+  ]);
+  private readonly router = inject(Router);
+
+  navigate(link: string): void {
+    this.router.navigate(['organization' + link]);
+  }
+}
